4beed067eeff4f1a5a358f135a61a0301c5941c5
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-11-29 17:53:58 +0300

    Add files via upload

--- models/task.py
+++ models/task.py
@@ -1,8 +1,8 @@
-from backend.db import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
-from models.user import User
-from sqlalchemy.schema import CreateTable
+from backend.db import Base
+from models import *
+
 
 class Task(Base):
     __tablename__ = 'tasks'
@@ -12,10 +12,7 @@ class Task(Base):
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
     slug = Column(String, unique=True, index=True)
 
-    user = relationship("User", back_populates="tasks")
-
-
-print(CreateTable(Task.__table__))
+    user = relationship('User', back_populates='tasks')
